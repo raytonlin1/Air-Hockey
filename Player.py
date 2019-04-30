468d21aaf1f41f5cc2f15337e235c123207401a9
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-04-29 21:58:39 -0400

    Update Player.py

--- Player.py
+++ Player.py
@@ -8,8 +8,8 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, img, id,  top, left, bottom, right):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((70, 70))
-        pygame.transform.scale(pygame.image.load(img).convert_alpha(), (70, 70), self.image)
+        self.image = pygame.Surface((50, 50))
+        pygame.transform.scale(pygame.image.load(img).convert_alpha(), (50, 50), self.image)
         self.image.set_colorkey(self.image.get_at((0,0)))
         self.rect = self.image.get_rect()
         self.rect.topleft = ((left+right)/2, (top+bottom)/2)
