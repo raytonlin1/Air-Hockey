f07673df54bd7e3de2ac38dd081fb5bb4ba4d4a2
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-04-30 01:29:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
-import pygame
 from Paddle import *
+from Puck import *
 from pygame.locals import *
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 640))
 p1 = Paddle("redpaddle.png", 0, 0, 0, 640, 500)
 p2 = Paddle("bluepaddle.png", 1, 0, 500, 640, 1000)
+puck = Puck("puck1.png", 0, 0, 640, 1000)
 background = pygame.Surface(screen.get_size()).convert()
+pygame.display.set_caption('Air Hockey')
 background.fill((255, 255, 255))
 clock = pygame.time.Clock()
 
@@ -24,4 +26,5 @@ while keep_going:
     screen.blit(background, (0, 0))
     screen.blit(p1.image, p1.rect)
     screen.blit(p2.image, p2.rect)
+    screen.blit(puck.image, puck.rect)
     pygame.display.flip()
