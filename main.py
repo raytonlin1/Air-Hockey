042d70b556870b220175914e449dc7a818ae793c
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-05-05 21:48:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from pygame.locals import *
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 640))
-p1 = Paddle("redpaddle.png", 1, 0, 0, 640, 500)
-p2 = Paddle("bluepaddle.png", 0, 0, 500, 640, 1000)
-puck = Puck("puck.png", 0, 0, 640, 1000)
+p1 = Paddle("redpaddle.png", 1, 0, 0, 640, 500, 50)
+p2 = Paddle("bluepaddle.png", 0, 0, 500, 640, 1000, 50)
+puck = Puck("puck.png", 0, 0, 640, 1000, 30)
 background = pygame.Surface(screen.get_size()).convert()
 pygame.display.set_caption('Air Hockey')
 background.fill((255, 255, 255))
