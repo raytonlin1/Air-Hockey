a5012bc86dd27d8dd48215a6ed38f6f8df1bad12
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-05-06 14:09:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ clock = pygame.time.Clock()
 
 keep_going = True
 
+recSize1 = (10,610)
+recSize2 = (1040,10)
+
+recSideLeft = pygame.Surface(recSize1).convert()
+recSideRight = pygame.Surface(recSize1).convert()
+recSideTop = pygame.Surface(recSize2).convert()
+recSideBottom = pygame.Surface(recSize2).convert()
+
 while keep_going:
     clock.tick(30)
     for ev in pygame.event.get():
@@ -28,9 +36,17 @@ while keep_going:
         puck.bounce(p1)
     if (p2.collide(puck)):
         puck.bounce(p2)
+    recSideLeft.fill((138,54,15))
+    recSideRight.fill((138,54,15))
+    recSideTop.fill((138,54,15))
+    recSideBottom.fill((138,54,15))
     puck.update()
     screen.blit(background, (0, 0))
     screen.blit(p1.image, p1.rect)
     screen.blit(p2.image, p2.rect)
     screen.blit(puck.image, puck.rect)
+    screen.blit(recSideLeft, (0,100))
+    screen.blit(recSideRight, (1030, 100))
+    screen.blit(recSideTop, (0, 90))
+    screen.blit(recSideBottom, (0, 690))
     pygame.display.flip()
