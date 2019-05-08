8a859c0360637b23957b1646b1604b58be99c24c
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-05-08 17:52:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ pygame.init()
 
 #screen = pygame.display.set_mode((1000, 640))
 screen = pygame.display.set_mode((1040, 700))
-p1 = Paddle("redpaddle.png", 1, 100, 10, 690, 520, 60)
-p2 = Paddle("bluepaddle.png", 0, 100, 520, 690, 1030, 60)
-puck = Puck("puck.png", 100, 10, 690, 1030, 45)
+p1 = Paddle("redpaddle.png", 1, 100, 10, 690, 520, 50)
+p2 = Paddle("bluepaddle.png", 0, 100, 520, 690, 1030, 50)
+puck = Puck("puck.png", 100, 10, 690, 1030, 30)
 background = pygame.Surface(screen.get_size()).convert()
 pygame.display.set_caption('Air Hockey')
 background.fill((255, 255, 255))
@@ -43,6 +43,7 @@ while keep_going:
         puck.bounce(p1)
     if (p2.collide(puck)):
         puck.bounce(p2)
+
     recSideLeft.fill((138,54,15))
     recSideRight.fill((138,54,15))
     recSideTop.fill((138,54,15))
@@ -50,7 +51,21 @@ while keep_going:
     goalLeft.fill((0,0,0))
     goalRight.fill((0,0,0))
     puck.update()
+    
     screen.blit(background, (0, 0))
+    # Centre line of air hockey surface
+    pygame.draw.line(screen, (255,0,0), (519,100), (519,690),5)
+    # Circles at centre of air hockey surface
+    pygame.draw.circle(screen, (255,0,0), (520, 395), 130)
+    pygame.draw.circle(screen, (255,255,255), (520, 395), 125)
+
+    # Semicircle at left goal
+    pygame.draw.circle(screen, (255,0,0), (0, 400), 120)
+    pygame.draw.circle(screen, (255,255,255), (0, 400), 115)
+
+    # Semicircle at right goal
+    pygame.draw.circle(screen, (255,0,0), (1040, 400), 120)
+    pygame.draw.circle(screen, (255,255,255), (1040, 400), 115)
     screen.blit(p1.image, p1.rect)
     screen.blit(p2.image, p2.rect)
     screen.blit(puck.image, puck.rect)
@@ -60,4 +75,5 @@ while keep_going:
     screen.blit(recSideBottom, (0,690))
     screen.blit(goalLeft, (0,(100+((700-100)/2)-(size1/2))))
     screen.blit(goalRight, (1030,(100+((700-100)/2)-(size2/2))))
+
     pygame.display.flip()
