a5ef56a9ea5dee843e3b994b90e5458359064231
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-05-12 04:54:17 -0400

    Update Goal.py

--- Goal.py
+++ Goal.py
@@ -3,20 +3,16 @@ from pygame.locals import *
 
 class Goal(pygame.sprite.Sprite):
 
-    def __init__(self, id, lt, tp, width):
+    def __init__(self, id, left, top, width):
         # Constructs the parent component
         pygame.sprite.Sprite.__init__(self)
         # Creates a goal
-        self.image = pygame.Surface((10,width)).convert()
+        self.image = pygame.Surface((30,width)).convert()
         # Colour of goal is black
         self.image.fill((0,0,0))
         # Determines the attributes of the goal as a rectangle
         self.rect = self.image.get_rect()
 
         # Sets the position of the goal
-        self.rect.left = lt
-        self.rect.top = tp
-
-    
-                                     
-        
+        self.rect.left = left
+        self.rect.top = top
